6692c822cb3b34f31897e218b7e209e17d74d967
Daniel <danielghost@live.com>
2024-04-16 16:40:57 +0800

    fix:bug

--- packge.sh
+++ packge.sh
@@ -1,3 +1,9 @@
 python3 -m build
 cp -r dist/* ../cflab_amhs/computing/dist/
+git add .
+git commit -m $1
+git pull
+git push 
+echo "push content: success!"
 sh ../cflab_amhs/git.sh "update package"
+echo "update cflab_amhs package: success!"
