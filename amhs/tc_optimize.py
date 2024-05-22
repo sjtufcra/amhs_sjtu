b2787354c94963f5ea77347852fde32033937d00
Daniel <danielghost@live.com>
2024-05-22 12:47:21 +0800

    update:new_file_fix_bu

--- amhs/tc_optimize.py
+++ amhs/tc_optimize.py
@@ -5,8 +5,8 @@ import concurrent.futures
 import multiprocessing
 from loguru import logger as log
 
-from tc_out import *
-from tc_in import *
+from .tc_out import *
+from .tc_in import *
 from .algorithm.A_start.graph.srccode import *
 
 def task_assign(p, use_multiprocessing=True):
