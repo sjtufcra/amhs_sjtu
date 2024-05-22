0c7270e10d814ea4c1030b5d816e664c237a15ee
Daniel <danielghost@live.com>
2024-05-22 12:36:55 +0800

    fix: bug

--- amhs/tc_main.py
+++ amhs/tc_main.py
@@ -13,9 +13,12 @@ class Amhs():
     def start(self):
         # if not self.loadBool():
         #     return 
+        
         d = Dataset(self.config)
         self.Node = d
+        log.info('init success')
         d = generating(d)
+        log.info('generating success')
         # await track_generate_station(d, d.original_map_info)
         # d = task_assign_new(d)
         d = task_assign(d)
