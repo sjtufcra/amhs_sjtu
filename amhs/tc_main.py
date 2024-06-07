616e9bf6c5f8d69fda114a0b7a1c1cc53cfcf2fc
Daniel <danielghost@live.com>
2024-06-07 17:37:57 +0800

    fix:bug

--- amhs/tc_main.py
+++ amhs/tc_main.py
@@ -1,4 +1,5 @@
 # online
+import time
 from .tc_class_sets import *
 from .tc_in import *
 # from .tc_assign import *
@@ -12,15 +13,10 @@ class Amhs():
         pass
     
     def start(self):
-        # if not self.loadBool():
-        #     return 
-        
         d = Dataset(self.config)
         self.Node = d
         d = generating(d)
-        # await track_generate_station(d, d.original_map_info)
-        # d = task_assign_new(d)
-        d = task_assign(d,d.use_multiprocessing)
+        d = epoch_static(d)
     
     def over(self):
         output_close_connection(self.d)
