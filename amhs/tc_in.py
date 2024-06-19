14f8908f92c8d52978a82502f7ec352c28addc62
Daniel <danielghost@live.com>
2024-06-19 18:47:18 +0800

    add new flode

--- amhs/tc_in.py
+++ amhs/tc_in.py
@@ -17,8 +17,6 @@ import copy
 from mysql import connector
 from loguru import logger as log
 from contextlib import contextmanager
-# from .tc_out import *
-# from .algorithm.A_start.graph.srccode import *
 
 from tc_out import *
 from algorithm.A_start.graph.srccode import *
