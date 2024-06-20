4778d438fbd755676655870f1c45684339b02266
Daniel <danielghost@live.com>
2024-06-20 10:53:49 +0800

    fix:bug

--- amhs/tc_out.py
+++ amhs/tc_out.py
@@ -4,16 +4,19 @@ from loguru import logger as log
 async def output_new(p, v):
     if p.debug_on:
         return None
-    sql0 = (f"UPDATE TRANSFER_TABLE "
-            f"SET VEHICLE = \'{v.vehicle_assigned}\', POSPATH = \'{','.join(v.delivery_route)} \'"
-            f"WHERE COMMANDID = \'{v.id}\'")
+    sql0 = (f'''UPDATE TRANSFER_TABLE 
+            SET VEHICLE = '{v.vehicle_assigned}', POSPATH = '{','.join(v.delivery_route)}'
+            WHERE COMMANDID = '{v.id}\'''')
     s0 = "SET VEHICLE = '" + v.vehicle_assigned + "', POSPATH = '" + ','.join(v.delivery_route)
     s1 = "' WHERE COMMANDID = '" + v.id + "'"
     sql = "UPDATE TRANSFER_TABLE " + s0 + s1
-    log.info(f'sql is: {sql}')
     with p.db_pool.get_connection() as db_conn:
         cursor = db_conn.cursor()
-        cursor.execute(sql)
+        try:
+            cursor.execute(sql0)
+        except Exception as e:
+            log.error(f'messge:sql0 is erro, erro:{e},change sql:{sql0}')
+            cursor.execute(sql)
         db_conn.commit()
         cursor.close()
     return None
