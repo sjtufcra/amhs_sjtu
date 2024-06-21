0570d4cc5efc1272dd17e7f196aace0ae5a54e5f
Daniel <danielghost@live.com>
2024-06-21 14:19:57 +0800

    update

--- amhs/test/reds.py
+++ amhs/test/reds.py
@@ -1,4 +1,4 @@
-import redisclusterlocal as rds
+from redspack import redisclusterlocal as rds
 import json
 pool = rds.ClusterConnectionPool(host='10.34.58.42', port=6379)
 connection = rds.RedisCluster(connection_pool=pool)
